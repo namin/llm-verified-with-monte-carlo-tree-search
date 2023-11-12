c0a531b7b60de46f5e9260edc5d130dd58e5903b
Nada Amin <namin@seas.harvard.edu>
2023-11-12 16:32:23 -0500

    precision

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ base_model_name = "Phind/Phind-CodeLlama-34B-v2"
 #base_model_name = "deepseek-ai/deepseek-coder-33b-instruct"
 # optional checkpoint
 peft_model_path = None
-# PPO model -- overrides the above
+# PPO model -- overrides all the above
 ppo_model_path = "./my_ppo_model"
 # set to None for run.py to instead use base model (and optionl peft)
 ppo_model_path = None
