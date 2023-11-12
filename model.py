6be62e49bfd1b2ab1e62e44c605e59c8b0e5e5eb
Nada Amin <namin@seas.harvard.edu>
2023-11-12 14:01:43 -0500

    try deepseek model

--- model.py
+++ model.py
@@ -1,7 +1,9 @@
 base_model_name = "Phind/Phind-CodeLlama-34B-v2"
 base_model_name = "codellama/CodeLlama-34b-hf"
+base_model_name = "deepseek-ai/deepseek-coder-33b-instruct"
 # optional checkpoint
 peft_model_path = None
+# PPO model -- overrides the above
 ppo_model_path = "./my_ppo_model"
 # set to None for run.py to instead use base model (and optionl peft)
 ppo_model_path = None
