2533e2fca5302c67a6e4f6edee2d0219e90e4f46
Nada Amin <namin@seas.harvard.edu>
2023-11-12 13:26:15 -0500

    try code llama

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 base_model_name = "Phind/Phind-CodeLlama-34B-v2"
+base_model_name = "codellama/CodeLlama-34b-hf"
 # optional checkpoint
 peft_model_path = None
 ppo_model_path = "./my_ppo_model"
 # set to None for run.py to instead use base model (and optionl peft)
-# ppo_model_path = None
+ppo_model_path = None
