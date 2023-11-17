7747ae3b580ff516661c937d60d1e90f42c17a85
Nada Amin <namin@seas.harvard.edu>
2023-11-16 21:12:17 -0500

    init from run_verifier_feedback

--- run_fail.py
+++ run_fail.py
@@ -3,7 +3,7 @@ import llm
 from montecarlo.node import Node
 from montecarlo.montecarlo import MonteCarlo
 
-from lang import score_func, can_be_solution
+from lang import score_func, can_be_solution, verifier_feedback
 
 from prompts import prompt, expansion_count, min_lines, check_fun
 from common import limit_depth, max_completion_depth
@@ -16,32 +16,33 @@ def generate_complete(text, montecarlo, current_completion_depth=1):
     text = llm.generate(text, 1)[0]
     score = score_func(text)
     if score is not None:
-        if score < 0:
-            return None
-        else:
-            if can_be_solution(text, min_lines, check_fun):
-                montecarlo.solution = text
-            return text
+        if score > 0 and can_be_solution(text, min_lines, check_fun):
+            montecarlo.solution = text
+        return (text, score)
     else:
         return generate_complete(text, montecarlo, current_completion_depth + 1)
-
 
 def child_finder(node, montecarlo):
     if limit_depth(node):
         return
 
-    text = generate_complete(node.state, montecarlo)
-    if text is None:
-        node.update_win_value(-1)
-    else:
-        child = Node(text)
-        node.add_child(child)
-        child.update_win_value(1)
-        child.update_policy_value(1)
+    (text, score) = generate_complete(node.state, montecarlo)
+    if score < 0:
+        hint = verifier_feedback(node.state, text)
+        if not hint:
+            node.update_win_value(-1)
+            return
+        else:
+            text = hint
 
-        child = Node(node.state)
-        node.add_child(child)
-        child.update_policy_value(0.2)
+    child = Node(text)
+    node.add_child(child)
+    child.update_win_value(1)
+    child.update_policy_value(1)
+
+    child = Node(node.state)
+    node.add_child(child)
+    child.update_policy_value(0.2)
 
 montecarlo.child_finder = child_finder
 
