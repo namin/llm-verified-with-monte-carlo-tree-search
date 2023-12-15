8cde4b3ec9f689eff665301c0cf7836e41a1600e
Nada Amin <namin@seas.harvard.edu>
2023-12-15 03:45:04 -0500

    tweaks

--- try_funsearch.py
+++ try_funsearch.py
@@ -5,4 +5,4 @@ f = open('funsearch/implementation/specification_nonsymmetric_admissible_set.txt
 PROMPT = f.read()
 f.close()
 
-main(PROMPT, [(10,15)], Config())
+main(PROMPT, [(10,5),(11,7),(12,7),(24,17),(15,10),(90,89)], Config())
