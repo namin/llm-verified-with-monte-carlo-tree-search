38a2db38ce0d2c69d3f9cc4fbbd540d5941130c6
Nada Amin <namin@seas.harvard.edu>
2023-12-17 04:07:24 -0500

    one more

--- prompts.py
+++ prompts.py
@@ -73,6 +73,73 @@ Proof.
     CHECK_PROOF, CHECK_CHEAT,
     ['Coq'])
 
+problem_partial_maps_proof_coq = (
+    """
+```Coq
+From Coq Require Import Arith.Arith.
+From Coq Require Import Bool.Bool.
+Require Import Coq.Strings.String.
+From Coq Require Import Logic.FunctionalExtensionality.
+From Coq Require Import Lists.List.
+Import ListNotations.
+Set Default Goal Selector "!".
+
+
+Definition total_map (A : Type) := string -> A.
+
+Definition t_empty {A : Type} (v : A) : total_map A :=
+  (fun _ => v).
+
+Definition t_update {A : Type} (m : total_map A)
+                    (x : string) (v : A) :=
+  fun x' => if String.eqb x x' then v else m x'.
+
+Notation "'_' '!->' v" := (t_empty v)
+  (at level 100, right associativity).
+
+Notation "x '!->' v ';' m" := (t_update m x v)
+                              (at level 100, v at next level, right associativity).
+
+Definition examplemap' :=
+  ( "bar" !-> true;
+    "foo" !-> true;
+    _ !-> false
+  ).
+
+Definition partial_map (A : Type) := total_map (option A).
+
+Definition empty {A : Type} : partial_map A :=
+  t_empty None.
+
+Definition update {A : Type} (m : partial_map A)
+           (x : string) (v : A) :=
+  (x !-> Some v ; m).
+
+Notation "x '|->' v ';' m" := (update m x v)
+  (at level 100, v at next level, right associativity).
+
+Notation "x '|->' v" := (update empty x v)
+  (at level 100).
+  
+Definition examplepmap :=
+  ("Church" |-> true ; "Turing" |-> false).
+
+Definition includedin {A : Type} (m m' : partial_map A) :=
+  forall x v, m x = Some v -> m' x = Some v.
+
+Theorem includedin_update : forall (A : Type) (m m' : partial_map A)
+                                 (x : string) (vx : A),
+includedin m m' ->
+includedin (x |-> vx ; m) (x |-> vx ; m').
+Proof.
+""",
+    1000,
+    None,
+    22,
+    40,
+    CHECK_PROOF, CHECK_CHEAT,
+    ['Coq'])
+
 problem_opt0_proof_coq = (
     """
 ```Coq
