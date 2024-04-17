0b4f0310aa0869ea1eb6233c83f40cb5639322e4
Nada Amin <namin@seas.harvard.edu>
2024-04-17 15:05:30 -0400

    reflexion

--- reflection.py
+++ reflection.py
@@ -38,7 +38,7 @@ def reflect(code: str, snippet: Optional[str], err: str) -> str:
     # adapted from F.3 Reflection Prompt
     # in https://arxiv.org/pdf/2310.04406v2.pdf
     prompt = f"""<s>[INST] <<SYS>>
-You are a {LANG} programming assistant. You will be given some code and an error. Your goal is to write a few sentences to explain why the is wrong as indicated by the error. You will need this as guidance when you try again later. Only provide the few sentence description in your answer, not the implementation.
+You are a {LANG} programming assistant. You will be given some code and an error. Your goal is to write a few sentences to explain why the code is wrong as indicated by the error. You will need this as guidance when you try again later. Only provide the few sentence description in your answer, not the implementation.
 <</SYS>>
 
 ```{LANG}
