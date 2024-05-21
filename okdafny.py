65674094d6ea25192c14903aa98a1abc5fa93c29
Simon Henniger <git@simonhenniger.info>
2024-05-21 16:04:32 -0400

    improve dafny script

--- okdafny.py
+++ okdafny.py
@@ -1,8 +1,12 @@
 weird = "lemma broken() ensures 1 == 0 {}"
+works = "lemma works() ensures 0 == 0 {}"
 
 from dafny import checkDafny
-if checkDafny(weird)["status"] == 1024:
-    print("Your Dafny install seems OK.")
+if checkDafny(weird)["status"] == 0:
+    print("Your verifier is accepting INCORRECT CODE. This causes results that are silently wrong! Try recompiling z3.")
 else:
-    print("Your verifier is likely BROKEN. Try recompiling z3.")
+    if checkDafny(works)["status"] == 0:
+        print("Your Dafny install seems OK.")
+    else:
+        print("Your Dafny install appears broken. Try running \"dafny\" on your own to fix it.")
 
