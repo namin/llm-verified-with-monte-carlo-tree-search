779544c57fd2106449cbb2a2e8c4bc83702b5c07
Nada Amin <namin@alum.mit.edu>
2024-06-04 16:28:28 -0400

    Update run.py
    Workaround to make test_dict compatible with legacy score_func.

--- run.py
+++ run.py
@@ -3,15 +3,19 @@ from montecarlo.montecarlo import MonteCarlo
 
 from lang import can_be_solution
 
-#from lang import score_func as uncached_score_func
-#from common_cache import create_cached_func
-#score_func, cache_stats, reset_cache = create_cached_func(uncached_score_func)
+from lang import score_func as uncached_score_func
 
 from lang import score_func
 
 from common_interactive import diffprompt
 
 from prompts import prompt, expansion_count, min_lines, check_func, test_dict
+if test_dict:
+    uncached_score_func_before_dict = uncached_score_func
+    uncached_score_func = lambda x: uncached_score_func_before_dict(x, test_dict)
+from common_cache import create_cached_func
+score_func, cache_stats, reset_cache = create_cached_func(uncached_score_func)
+
 from common import limit_depth, max_completion_depth
 from common_stats import stats
 
@@ -23,7 +27,7 @@ def generate_complete(text, montecarlo, current_completion_depth=1):
     prev = text
     texts = llm.generate(text, 1)
     text = texts[0]
-    score = score_func(text, test_dict)
+    score = score_func(text)
     print(diffprompt(prev, texts))
     if score is not None:
         if score < 0:
