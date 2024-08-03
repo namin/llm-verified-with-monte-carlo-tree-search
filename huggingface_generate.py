3e3b0cf2af6c962657a7ec2b73b5197046866644
Nada Amin <namin@seas.harvard.edu>
2024-08-02 20:20:47 -0400

    this change prolly does not hurt

--- huggingface_generate.py
+++ huggingface_generate.py
@@ -51,10 +51,12 @@ def load_model(
         load_in_4bit=True,
         bnb_4bit_quant_type="nf4",
         bnb_4bit_compute_dtype=torch.float16,
+        bnb_4bit_use_double_quant=True,
     )
     if ppo_model_path is None:
         base_model = AutoModelForCausalLM.from_pretrained(
             base_model_name,
+            torch_dtype=torch.bfloat16,
             quantization_config=bnb_config,
             device_map="auto",
             trust_remote_code=True,
